e7b2205916c391e87c083114d5c130e02680609f
Milo Wyner <milowyner@gmail.com>
2017-01-17 17:18:00 -0800

    Key presses will now also play audio

--- javascript-drum-kit/script.js
+++ javascript-drum-kit/script.js
@@ -1,17 +1,27 @@
 var key;
 var keyElement;
+var audio;
 
 window.onload = init;
 
 function init() {
   window.addEventListener("keydown", function(event) {
-    console.log("keydown");
-    key = event.keyCode;
-    keyElement = document.querySelector("div[data-key=\"" + key + "\"]");
+    setKeyVariables(event);
     keyElement.className += " playing";
+    audio = document.querySelector("audio[data-key=\"" + key + "\"]");
+    if (!audio.paused) { // Stops sound if it's currently playing
+      audio.pause();
+      audio.currentTime = 0;
+    }
+    audio.play();
   });
   window.addEventListener("keyup", function(event) {
-    console.log("keyup");
+    setKeyVariables(event);
     keyElement.className = "key";
   });
+}
+
+function setKeyVariables(event) {
+  key = event.keyCode;
+  keyElement = document.querySelector("div[data-key=\"" + key + "\"]");
 }
